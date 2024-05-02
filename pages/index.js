3e47c2a956a14070a14af13ae46eb4b14d8ab13a
Simone Anthony <s.anthony@tier1performance.com>
2024-05-02 15:55:12 -0400

    meetup app ready for deployment

---
+++ pages/index.js
@@ -0,0 +1,73 @@
+import Head from 'next/head';
+import { MongoClient } from 'mongodb';
+
+import MeetupList from "@/components/meetups/MeetupList";
+import { Fragment, useEffect, useState } from "react";
+
+// const DUMMY_MEETUPS = [
+//     {
+//         id: 'm1',
+//         title: 'A First Meetup',
+//         image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+//         address: 'Some address 5, 12345 Some City',
+//         description: 'This is a first meetup!'
+//     },
+//     {
+//         id: 'm2',
+//         title: 'A Second Meetup',
+//         image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+//         address: 'Some address 10, 12345 Some City',
+//         description: 'This is a second meetup!'
+//     },
+// ]
+
+function HomePage(props) {
+    return (
+        <Fragment>
+            <Head>
+                <title>React Meetups</title>
+                <meta name="description" content="Browse a huge list of highly active React meetups!" />
+            </Head>
+        <MeetupList meetups={props.meetups} />
+        </Fragment>
+    );
+}
+
+// export async function getServerSideProps(context) {
+//     const req = context.req;
+//     const res = context.res;
+    
+//     // fetch data from an API
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+//         }
+//     };
+// }
+
+export async function getStaticProps() {
+    // fetch data from an API
+    const client = await MongoClient.connect('mongodb+srv://admin:root@cluster1.jvbwbdp.mongodb.net/meetups?retryWrites=true&w=majority');
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const meetups = await meetupsCollection.find().toArray();
+
+    client.close();
+
+    return {
+        props: {
+            meetups: meetups.map((meetup) => ({
+                id: meetup._id.toString(),
+                title: meetup.title,
+                address: meetup.address,
+                image: meetup.image,
+            }))
+        },
+        revalidate: 1,
+    };
+}
+
+export default HomePage;
